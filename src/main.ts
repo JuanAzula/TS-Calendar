ce8258080ac4b0711cc355f3124a322b5a44cf87
JuanAzula <141027910+JuanAzula@users.noreply.github.com>
2024-01-25 08:49:19 +0100

    Close modal by clicking outside

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 // ///////// EVENT LISTENERS FOR ADD EVENT BUTTON
 const eventButton = document.getElementById("add-event");
+const label = document.querySelector("#add-event .header__button--text");
 
 if (eventButton && eventButton instanceof HTMLButtonElement) {
   eventButton.addEventListener("click", () => {
@@ -8,6 +9,15 @@ if (eventButton && eventButton instanceof HTMLButtonElement) {
   })
 }
 
+if (label && label instanceof HTMLSpanElement) {
+  label.addEventListener("click", () => {
+    const modal = document.getElementById("modal");
+    modal?.classList.remove("hide-modal");
+  })
+}
+
+
+// ///////// EVENT LISTENERS FOR CLOSE BUTTON
 const closeModal = document.getElementById("close");
 
 if (closeModal && closeModal instanceof HTMLButtonElement) {
@@ -16,3 +26,20 @@ if (closeModal && closeModal instanceof HTMLButtonElement) {
     modal?.classList.add("hide-modal");
   })
 }
+
+// //////// EVENT LISTENERS FOR CLOSE MODAL
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    const modal = document.getElementById("modal");
+    modal?.classList.add("hide-modal");
+  }
+})
+
+document.addEventListener("click", (event) => {
+  const modal = document.getElementById("modal");
+  if (modal !== null && event.target !== null) {
+    if (event.target !== modal && event.target !== eventButton && event.target !== label) {
+      modal.classList.add("hide-modal");
+    }
+  }
+});
