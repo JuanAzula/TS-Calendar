937f5d9abf5cb0d5061a26ada567312faff285a8
Javiermartin12 <153718754+Javiermartin12@users.noreply.github.com>
2024-01-26 10:24:53 +0100

    Dont know

---
+++ src/components/Calendar.ts
@@ -0,0 +1,53 @@
+import { getEvents } from "./Modal";
+
+enum Months {
+
+    January = 0,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
+
+export function createCalendar(month: Months, year: number): void {
+    const firstDay: number = new Date(year, month, 1).getDay();
+    const firstDayAdjusted: number = (firstDay === 0) ? 6 : firstDay - 1;
+    const daysInMonth: number = new Date(year, month + 1, 0).getDate();
+
+    const calendarGrid: HTMLElement | null = document.getElementById('calendar-container');
+
+    if (calendarGrid) {
+        calendarGrid.innerHTML = ''; // Neteja la graella si ja té elements
+
+        // Omplir els dies anteriors al primer dia del mes
+        for (let i = 0; i < firstDayAdjusted; i++) {
+            let emptyCell = document.createElement('div');
+            calendarGrid.appendChild(emptyCell);
+        }
+
+        // Omplir amb els dies del mes
+        for (let day = 1; day <= daysInMonth; day++) {
+            let dayCell = document.createElement('div');
+            dayCell?.classList.add("day-item");
+            dayCell.textContent = day.toString().padStart(2, '0');
+
+
+            const monthValue = (month).toString().padStart(1, '0'); // Asegurarse de que el mes tenga al menos dos dígitos
+            const dateValue = `${year}-${monthValue + 1}-${day.toString().padStart(2, '0')}`;
+            dayCell.setAttribute("data-date", dateValue);
+            calendarGrid.appendChild(dayCell);
+            console.log(dayCell.getAttribute("data-date"))
+        }
+        getEvents();
+    }
+}
+
+// Pots cridar aquesta funció amb el mes i any actuals
+createCalendar(new Date().getMonth(), new Date().getFullYear());
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,16 @@
+
+import { createCalendar } from "./components/Calendar"
+import { StoreEvent } from "./components/Modal";
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  // getEvents();
+})
+
+
+
+
 // ///////// EVENT LISTENERS FOR ADD EVENT BUTTON
 const eventButton = document.getElementById("add-event");
 
@@ -12,7 +25,57 @@ const closeModal = document.getElementById("close");
 
 if (closeModal && closeModal instanceof HTMLButtonElement) {
   closeModal?.addEventListener("click", () => {
+
     const modal = document.getElementById("modal");
     modal?.classList.add("hide-modal");
   })
 }
+
+    overlay?.classList.add("hide-modal");
+    modal?.classList.add("hide-modal");
+  })
+}
+
+// ///////// CALENDAR
+createCalendar(new Date().getMonth(), new Date().getFullYear());
+
+
+// //////// EVENT LISTENERS FOR CLOSE MODAL
+document.addEventListener("keydown", (event) => {
+  if (overlay && overlay instanceof HTMLDivElement) {
+    overlay?.addEventListener("click", () => {
+      overlay.classList.add("hide-modal");
+      modal?.classList.add("hide-modal");
+    })
+  }
+  if (event.key === "Escape") {
+    const modal = document.getElementById("modal");
+    modal?.classList.add("hide-modal");
+  }
+})
+
+//  /////// EVENT LISTENERS FOR SUBMIT BUTTON
+
+const submitButton = document.getElementById("submit");
+const title = document.getElementById("title");
+const date = document.getElementById("date");
+const time = document.getElementById("time");
+
+if (submitButton && submitButton instanceof HTMLButtonElement && title && title instanceof HTMLInputElement && date && date instanceof HTMLInputElement && time && time instanceof HTMLInputElement) {
+  submitButton?.addEventListener("click", (event) => {
+    console.log("hola")
+    event?.preventDefault();
+    const titleValue = title?.value;
+    const dateValue = date?.value;
+    const timeValue = time?.value;
+    console.log(titleValue, dateValue, timeValue)
+    StoreEvent(titleValue, dateValue, timeValue);
+    overlay?.classList.add("hide-modal");
+    modal?.classList.add("hide-modal");
+  })
+}
+
+//  /////// EVENT LISTENERS FOR CHECKBOX
+
+const endDateCheckbox = document.getElementById('checkbox');
+const endDateLabel = document.getElementById('endDate-label');
