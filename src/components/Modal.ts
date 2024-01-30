71c134c1ec4c250cfc1e59da39aba28fa0ff38ae
JuanAzula <141027910+JuanAzula@users.noreply.github.com>
2024-01-30 19:41:53 +0100

    Style events modal

--- src/components/Modal.ts
+++ src/components/Modal.ts
@@ -75,6 +75,7 @@ export function getEvents() {
 
         if (isPastEvent) {
             eventDiv.classList.add("past-event");
+            console.log("is past event");
         }
 
         if (eventsTotal) {
